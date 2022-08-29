f5e1a818a1c466fabbb454ffc1124adc9075d62b
yucarl77 <yucarl77@foxmail.com>
2022-08-29 16:47:43 +0800

    refactor: player and gamemode

--- src/controllers/gamemode.ts
+++ src/controllers/gamemode.ts
@@ -2,38 +2,33 @@ import { OnGameModeExit, OnGameModeInit } from "@/wrapper/callbacks";
 import { GameModeExit } from "@/wrapper/functions";
 
 export default class BaseGameMode {
-  private static instance: BaseGameMode;
   private initialized = false;
 
-  private constructor() {}
+  public constructor() {
+    OnGameModeInit((): void => this.OnInit());
+    OnGameModeExit((): void => this.OnExit());
+  }
 
-  public static getInstance(): BaseGameMode {
-    if (!BaseGameMode.instance) BaseGameMode.instance = new BaseGameMode();
-    return BaseGameMode.instance;
+  get isInitialized(): boolean {
+    return this.initialized;
   }
 
   // do something during initialization, such as load some objects
   // final callback to main.ts
-  public init(func: () => void): void {
+  public init(): void {
     if (this.initialized)
       throw new Error("[GameMode]: Cannot be initialized more than once");
     this.initialized = true;
-    this.OnInit(func);
-  }
-
-  public OnInit(callback: () => void) {
-    OnGameModeInit((): void => callback());
   }
 
   // do something during close/restart server, such as storage of player data
-  public exit(func: () => void): void {
-    if (!this.initialized) return;
+  public exit(): void {
+    if (!this.initialized)
+      throw new Error("[GameMode]: Cannot be unload more than once");
     this.initialized = false;
     GameModeExit();
-    this.OnExit(func);
   }
 
-  public OnExit(callback: () => void) {
-    OnGameModeExit((): void => callback());
-  }
+  public OnInit(): void {}
+  public OnExit(): void {}
 }
--- src/controllers/player.ts
+++ src/controllers/player.ts
@@ -5,22 +5,38 @@ import {
   SetPlayerDrunkLevel,
 } from "@/wrapper/functions";
 
-interface ISettings {
+export interface IPlayerSettings {
   locale: string;
   charset: string;
 }
+
+OnPlayerConnect((playerid: number): void => {
+  const p = new BasePlayer();
+  p.id = playerid;
+  BasePlayer.players.push(p);
+  p.OnConnect();
+});
+
+OnPlayerDisconnect((playerid: number): void => {
+  const p = BasePlayer.findPlayerIdx(playerid);
+  if (p === -1) return;
+  BasePlayer.players[p].OnDisconnect();
+  BasePlayer.players.splice(p, 1);
+});
+
 export class BasePlayer {
   public static players: Array<BasePlayer> = [];
-  public id: number;
+  public id = -1;
   public name = "";
-  public settings: ISettings;
+  public settings: IPlayerSettings = { locale: "", charset: "" };
   private lastDrunkLevel = 0;
   private lastFps = 0;
 
-  constructor(id: number, settings: ISettings) {
-    this.id = id;
-    this.settings = settings;
-  }
+  public constructor() {}
+
+  // todo: need outside default locale value
+  public OnConnect(): void {}
+  public OnDisconnect(): void {}
 
   get isNpc(): boolean {
     return Boolean(IsPlayerNPC(this.id));
@@ -52,24 +68,6 @@ export class BasePlayer {
   }
   set locale(language: string) {
     this.settings.locale = language;
-  }
-
-  public static OnConnect(fn: (p: BasePlayer) => void): void {
-    OnPlayerConnect((playerid: number) => {
-      // todo: need outside default value
-      const p = new BasePlayer(playerid, { locale: "", charset: "" });
-      BasePlayer.players.push(p);
-      fn(p);
-    });
-  }
-
-  public static OnDisconnect(fn: (p: BasePlayer) => void): void {
-    OnPlayerDisconnect((playerid: number) => {
-      const p = BasePlayer.findPlayerIdx(playerid);
-      if (p === -1) return;
-      fn(BasePlayer.players[p]);
-      BasePlayer.players.splice(p, 1);
-    });
   }
 
   public static findPlayer(playerid: number): BasePlayer | undefined {
