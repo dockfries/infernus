a9940a3feed74e76f439e4996d6b4d225babf615
yucarl77 <yucarl77@foxmail.com>
2022-09-03 00:19:48 +0800

    refactor: improve vehicle constructor

--- src/controllers/vehicle/vehicleEvent.ts
+++ src/controllers/vehicle/vehicleEvent.ts
@@ -3,19 +3,23 @@ import { BasePlayer } from "../player";
 import { BaseVehicle } from "./baseVehicle";
 
 abstract class VehicleEvent<V extends BaseVehicle, P extends BasePlayer> {
+  protected players: Array<P>;
+  constructor(players: Array<P>) {
+    this.players = players;
+  }
   protected abstract onDamageStatusUpdate(vehicle: V, player: P): void;
 }
 
-// Need a way to get an array of all players of the specified player event class
 export abstract class BaseVehicleEvent<
   V extends BaseVehicle,
   P extends BasePlayer
 > extends VehicleEvent<V, P> {
-  constructor() {
-    super();
+  // Need a way to get an array of all players of the specified player event class
+  constructor(players: Array<P>) {
+    super(players);
     // OnVehicleDamageStatusUpdate((vehicleid: number, playerid: number) => {
-    // const veh: BaseVehicle = this.vehicles.find((v) => v.id === vehicle.id);
-    // const player: BasePlayer = players.find.....
+    // const veh: BaseVehicle = this.vehicles.find((v) => v.id === vehicleid);
+    // const player: BasePlayer = this.players.find((p) => p.id === playerid);
     //   this.onDamageStatusUpdate(veh, player);
     // });
   }
