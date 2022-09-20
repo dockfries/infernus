2580df4cd979f70ff03f1f28e757fdec43ed7d80
yucarl77 <yucarl77@foxmail.com>
2022-09-20 21:17:03 +0800

    feat: streamer data manipulation

--- src/controllers/streamer/common/index.ts
+++ src/controllers/streamer/common/index.ts
@@ -1,204 +1,141 @@
 import { BasePlayer } from "@/controllers/player";
-import {
-  StreamerItemTypes,
-  Streamer_AmxUnloadDestroyItems,
-  Streamer_CountItems,
-  Streamer_CountVisibleItems,
-  Streamer_DestroyAllItems,
-  Streamer_DestroyAllVisibleItems,
-  Streamer_GetAllVisibleItems,
-  Streamer_GetCellDistance,
-  Streamer_GetCellSize,
-  Streamer_GetChunkSize,
-  Streamer_GetChunkTickRate,
-  Streamer_GetDistanceToItem,
-  Streamer_GetItemInternalID,
-  Streamer_GetItemOffset,
-  Streamer_GetItemPos,
-  Streamer_GetItemStreamerID,
-  Streamer_GetLastUpdateTime,
-  Streamer_GetMaxItems,
-  Streamer_GetNearbyItems,
-  Streamer_GetPlayerTickRate,
-  Streamer_GetRadiusMultiplier,
-  Streamer_GetTickRate,
-  Streamer_GetTypePriority,
-  Streamer_GetVisibleItems,
-  Streamer_IsItemVisible,
-  Streamer_IsToggleCameraUpdate,
-  Streamer_IsToggleChunkStream,
-  Streamer_IsToggleErrorCallback,
-  Streamer_IsToggleIdleUpdate,
-  Streamer_IsToggleItem,
-  Streamer_IsToggleItemCallbacks,
-  Streamer_IsToggleItemInvAreas,
-  Streamer_IsToggleItemStatic,
-  Streamer_IsToggleItemUpdate,
-  Streamer_OnItemStreamIn,
-  Streamer_OnItemStreamOut,
-  Streamer_OnPluginError,
-  Streamer_ProcessActiveItems,
-  Streamer_SetCellDistance,
-  Streamer_SetCellSize,
-  Streamer_SetChunkSize,
-  Streamer_SetChunkTickRate,
-  Streamer_SetItemOffset,
-  Streamer_SetItemPos,
-  Streamer_SetMaxItems,
-  Streamer_SetPlayerTickRate,
-  Streamer_SetRadiusMultiplier,
-  Streamer_SetTickRate,
-  Streamer_SetTypePriority,
-  Streamer_SetVisibleItems,
-  Streamer_ToggleAllItems,
-  Streamer_ToggleCameraUpdate,
-  Streamer_ToggleChunkStream,
-  Streamer_ToggleErrorCallback,
-  Streamer_ToggleIdleUpdate,
-  Streamer_ToggleItem,
-  Streamer_ToggleItemCallbacks,
-  Streamer_ToggleItemInvAreas,
-  Streamer_ToggleItemStatic,
-  Streamer_ToggleItemUpdate,
-  Streamer_Update,
-  Streamer_UpdateEx,
-} from "omp-wrapper-streamer";
+import * as ows from "omp-wrapper-streamer";
 
 export abstract class Streamer {
   constructor() {
-    Streamer_OnItemStreamIn(this.onItemStreamIn);
-    Streamer_OnItemStreamOut(this.onItemStreamOut);
-    Streamer_OnPluginError(this.onPluginError);
+    ows.Streamer_OnItemStreamIn(this.onItemStreamIn);
+    ows.Streamer_OnItemStreamOut(this.onItemStreamOut);
+    ows.Streamer_OnPluginError(this.onPluginError);
   }
-  public static getTickRate = Streamer_GetTickRate;
-  public static setTickRate = Streamer_SetTickRate;
+  public static getTickRate = ows.Streamer_GetTickRate;
+  public static setTickRate = ows.Streamer_SetTickRate;
   public static getPlayerTickRate<P extends BasePlayer>(player: P): number {
-    return Streamer_GetPlayerTickRate(player.id);
+    return ows.Streamer_GetPlayerTickRate(player.id);
   }
   public static setPlayerTickRate<P extends BasePlayer>(
     player: P,
     rate = 50
   ): number {
-    return Streamer_SetPlayerTickRate(player.id, rate);
+    return ows.Streamer_SetPlayerTickRate(player.id, rate);
   }
-  public static toggleChunkStream = Streamer_ToggleChunkStream;
-  public static isToggleChunkStream = Streamer_IsToggleChunkStream;
+  public static toggleChunkStream = ows.Streamer_ToggleChunkStream;
+  public static isToggleChunkStream = ows.Streamer_IsToggleChunkStream;
   public static getChunkTickRate<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_GetChunkTickRate(type, player.id);
+      return ows.Streamer_GetChunkTickRate(type, player.id);
     }
-    return Streamer_GetChunkTickRate(type, player);
+    return ows.Streamer_GetChunkTickRate(type, player);
   }
   public static setChunkTickRate<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     rate: number,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_SetChunkTickRate(type, rate, player.id);
+      return ows.Streamer_SetChunkTickRate(type, rate, player.id);
     }
-    return Streamer_SetChunkTickRate(type, rate, player);
+    return ows.Streamer_SetChunkTickRate(type, rate, player);
   }
-  public static getChunkSize = Streamer_GetChunkSize;
-  public static setChunkSize = Streamer_SetChunkSize;
-  public static getMaxItems = Streamer_GetMaxItems;
-  public static setMaxItems = Streamer_SetMaxItems;
+  public static getChunkSize = ows.Streamer_GetChunkSize;
+  public static setChunkSize = ows.Streamer_SetChunkSize;
+  public static getMaxItems = ows.Streamer_GetMaxItems;
+  public static setMaxItems = ows.Streamer_SetMaxItems;
   public static getVisibleItems<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_GetVisibleItems(type, player.id);
+      return ows.Streamer_GetVisibleItems(type, player.id);
     }
-    return Streamer_GetVisibleItems(type, player);
+    return ows.Streamer_GetVisibleItems(type, player);
   }
   public static setVisibleItems<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     items: number,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_SetVisibleItems(type, items, player.id);
+      return ows.Streamer_SetVisibleItems(type, items, player.id);
     }
-    return Streamer_SetVisibleItems(type, items, player);
+    return ows.Streamer_SetVisibleItems(type, items, player);
   }
   public static getRadiusMultiplier<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_GetRadiusMultiplier(type, player.id);
+      return ows.Streamer_GetRadiusMultiplier(type, player.id);
     }
-    return Streamer_GetRadiusMultiplier(type, player);
+    return ows.Streamer_GetRadiusMultiplier(type, player);
   }
   public static setRadiusMultiplier<P extends BasePlayer>(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     multiplier: number,
     player: number | P = -1
   ): number {
     if (player instanceof BasePlayer) {
-      return Streamer_SetRadiusMultiplier(type, player.id);
+      return ows.Streamer_SetRadiusMultiplier(type, player.id);
     }
-    return Streamer_SetRadiusMultiplier(type, multiplier, player);
+    return ows.Streamer_SetRadiusMultiplier(type, multiplier, player);
   }
-  public static getTypePriority = Streamer_GetTypePriority;
-  public static setTypePriority = Streamer_SetTypePriority;
-  public static getCellDistance = Streamer_GetCellDistance;
-  public static setCellDistance = Streamer_SetCellDistance;
-  public static getCellSize = Streamer_GetCellSize;
-  public static setCellSize = Streamer_SetCellSize;
-  public static toggleItemStatic = Streamer_ToggleItemStatic;
-  public static isToggleItemStatic = Streamer_IsToggleItemStatic;
-  public static toggleItemInvAreas = Streamer_ToggleItemInvAreas;
-  public static isToggleItemInvAreas = Streamer_IsToggleItemInvAreas;
-  public static toggleItemCallbacks = Streamer_ToggleItemCallbacks;
-  public static isToggleItemCallbacks = Streamer_IsToggleItemCallbacks;
-  public static toggleErrorCallback = Streamer_ToggleErrorCallback;
-  public static isToggleErrorCallback = Streamer_IsToggleErrorCallback;
-  public static amxUnloadDestroyItems = Streamer_AmxUnloadDestroyItems;
-  public static processActiveItems = Streamer_ProcessActiveItems;
+  public static getTypePriority = ows.Streamer_GetTypePriority;
+  public static setTypePriority = ows.Streamer_SetTypePriority;
+  public static getCellDistance = ows.Streamer_GetCellDistance;
+  public static setCellDistance = ows.Streamer_SetCellDistance;
+  public static getCellSize = ows.Streamer_GetCellSize;
+  public static setCellSize = ows.Streamer_SetCellSize;
+  public static toggleItemStatic = ows.Streamer_ToggleItemStatic;
+  public static isToggleItemStatic = ows.Streamer_IsToggleItemStatic;
+  public static toggleItemInvAreas = ows.Streamer_ToggleItemInvAreas;
+  public static isToggleItemInvAreas = ows.Streamer_IsToggleItemInvAreas;
+  public static toggleItemCallbacks = ows.Streamer_ToggleItemCallbacks;
+  public static isToggleItemCallbacks = ows.Streamer_IsToggleItemCallbacks;
+  public static toggleErrorCallback = ows.Streamer_ToggleErrorCallback;
+  public static isToggleErrorCallback = ows.Streamer_IsToggleErrorCallback;
+  public static amxUnloadDestroyItems = ows.Streamer_AmxUnloadDestroyItems;
+  public static processActiveItems = ows.Streamer_ProcessActiveItems;
   public static toggleIdleUpdate<P extends BasePlayer>(
     player: P,
     toggle: boolean
   ): number {
-    return Streamer_ToggleIdleUpdate(player.id, toggle);
+    return ows.Streamer_ToggleIdleUpdate(player.id, toggle);
   }
   public static isToggleIdleUpdate<P extends BasePlayer>(player: P): boolean {
-    return Streamer_IsToggleIdleUpdate(player.id);
+    return ows.Streamer_IsToggleIdleUpdate(player.id);
   }
   public static toggleCameraUpdate<P extends BasePlayer>(
     player: P,
     toggle: boolean
   ): number {
-    return Streamer_ToggleCameraUpdate(player.id, toggle);
+    return ows.Streamer_ToggleCameraUpdate(player.id, toggle);
   }
   public static isToggleCameraUpdate<P extends BasePlayer>(player: P): boolean {
-    return Streamer_IsToggleCameraUpdate(player.id);
+    return ows.Streamer_IsToggleCameraUpdate(player.id);
   }
   public static toggleItemUpdate<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     toggle: boolean
   ): number {
-    return Streamer_ToggleItemUpdate(player.id, type, toggle);
+    return ows.Streamer_ToggleItemUpdate(player.id, type, toggle);
   }
   public static isToggleItemUpdate<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes
+    type: ows.StreamerItemTypes
   ): boolean {
-    return Streamer_IsToggleItemUpdate(player.id, type);
+    return ows.Streamer_IsToggleItemUpdate(player.id, type);
   }
   public static getLastUpdateTime(): number {
-    return Streamer_GetLastUpdateTime();
+    return ows.Streamer_GetLastUpdateTime();
   }
   public static update<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes | -1 = -1
+    type: ows.StreamerItemTypes | -1 = -1
   ): number {
-    return Streamer_Update(player.id, type);
+    return ows.Streamer_Update(player.id, type);
   }
   public static updateEx<P extends BasePlayer>(
     player: P,
@@ -207,11 +144,11 @@ export abstract class Streamer {
     z: number,
     worldid?: number,
     interiorid?: number,
-    type?: StreamerItemTypes | -1,
+    type?: ows.StreamerItemTypes | -1,
     compensatedtime?: number,
     freezeplayer?: boolean
   ): number {
-    return Streamer_UpdateEx(
+    return ows.Streamer_UpdateEx(
       player.id,
       x,
       y,
@@ -223,86 +160,98 @@ export abstract class Streamer {
       freezeplayer
     );
   }
-  public static getDistanceToItem = Streamer_GetDistanceToItem;
+  public static getDistanceToItem = ows.Streamer_GetDistanceToItem;
   public static toggleItem<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     id: number,
     toggle: boolean
   ): number {
-    return Streamer_ToggleItem(player.id, type, id, toggle);
+    return ows.Streamer_ToggleItem(player.id, type, id, toggle);
   }
   public static isToggleItem<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     id: number
   ): boolean {
-    return Streamer_IsToggleItem(player.id, type, id);
+    return ows.Streamer_IsToggleItem(player.id, type, id);
   }
   public static toggleAllItems<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     toggle: boolean,
     exceptions: number[] = [-1]
   ): number {
-    return Streamer_ToggleAllItems(player.id, type, toggle, exceptions);
+    return ows.Streamer_ToggleAllItems(player.id, type, toggle, exceptions);
   }
   public static getItemInternalID<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     streamerid: number
   ): number {
-    return Streamer_GetItemInternalID(player.id, type, streamerid);
+    return ows.Streamer_GetItemInternalID(player.id, type, streamerid);
   }
   public static getItemStreamerID<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     internalid: number
   ): number {
-    return Streamer_GetItemStreamerID(player.id, type, internalid);
+    return ows.Streamer_GetItemStreamerID(player.id, type, internalid);
   }
   public static isItemVisible<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     id: number
   ): boolean {
-    return Streamer_IsItemVisible(player.id, type, id);
+    return ows.Streamer_IsItemVisible(player.id, type, id);
   }
   public static destroyAllVisibleItems<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     serverwide = 1
   ): number {
-    return Streamer_DestroyAllVisibleItems(player.id, type, serverwide);
+    return ows.Streamer_DestroyAllVisibleItems(player.id, type, serverwide);
   }
   public static countVisibleItems<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     serverwide = 1
   ): number {
-    return Streamer_CountVisibleItems(player.id, type, serverwide);
+    return ows.Streamer_CountVisibleItems(player.id, type, serverwide);
   }
-  public static destroyAllItems = Streamer_DestroyAllItems;
-  public static countItems = Streamer_CountItems;
-  public static getNearbyItems = Streamer_GetNearbyItems;
+  public static destroyAllItems = ows.Streamer_DestroyAllItems;
+  public static countItems = ows.Streamer_CountItems;
+  public static getNearbyItems = ows.Streamer_GetNearbyItems;
   public static getAllVisibleItems<P extends BasePlayer>(
     player: P,
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     items: number[]
   ): void {
-    Streamer_GetAllVisibleItems(player.id, type, items);
+    ows.Streamer_GetAllVisibleItems(player.id, type, items);
   }
-  public static getItemPos = Streamer_GetItemPos;
-  public static setItemPos = Streamer_SetItemPos;
-  public static getItemOffset = Streamer_GetItemOffset;
-  public static setItemOffset = Streamer_SetItemOffset;
+  public static getItemPos = ows.Streamer_GetItemPos;
+  public static setItemPos = ows.Streamer_SetItemPos;
+  public static getItemOffset = ows.Streamer_GetItemOffset;
+  public static setItemOffset = ows.Streamer_SetItemOffset;
+  public static getFloatData = ows.Streamer_GetFloatData;
+  public static setFloatData = ows.Streamer_SetFloatData;
+  public static getIntData = ows.Streamer_GetIntData;
+  public static setIntData = ows.Streamer_SetIntData;
+  public static getArrayData = ows.Streamer_GetArrayData;
+  public static setArrayData = ows.Streamer_SetArrayData;
+  public static isInArrayData = ows.Streamer_IsInArrayData;
+  public static appendArrayData = ows.Streamer_AppendArrayData;
+  public static removeArrayData = ows.Streamer_RemoveArrayData;
+  public static getArrayDataLength = ows.Streamer_GetArrayDataLength;
+  public static getUpperBound = ows.Streamer_GetUpperBound;
+
   public abstract onItemStreamIn(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     id: number,
     forplayerid: number
   ): number;
   public abstract onItemStreamOut(
-    type: StreamerItemTypes,
+    type: ows.StreamerItemTypes,
     id: number,
     forplayerid: number
   ): number;
