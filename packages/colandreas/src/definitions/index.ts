115605956dfe314a3da5d95f179ae23861128a47
dockfries <yucarl77@foxmail.com>
2025-08-08 23:51:41 +0800

    feat(colandreas): global defined

--- packages/colandreas/src/definitions/index.ts
+++ packages/colandreas/src/definitions/index.ts
@@ -1,3 +1,9 @@
+if (!samp.defined) {
+  samp.defined = {};
+}
+
+samp.defined._colandreas_included = true;
+
 export const MAX_CA_OBJECTS = 50000;
 export const WATER_OBJECT = 20000;
 export const FLOAT_INFINITY = 0x7f800000;
--- packages/colandreas/src/main.ts
+++ packages/colandreas/src/main.ts
@@ -1,3 +1,4 @@
+import "./definitions";
 export * from "./definitions";
 export * from "./natives";
 export * from "./functions";
